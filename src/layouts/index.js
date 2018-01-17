fdeff38c67d0ecff3cc4a2fc00d2af0e515c45df
Paula Lavalle <lavalle.paula@gmail.com>
2018-01-17 12:45:48 -0800

    Remove unnecessary getLocalTitle() function

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,33 +6,13 @@ import config from "../../data/site-config";
 import "../styles/styles.css";
 
 class MainLayout extends React.Component {
-  getLocalTitle() {
-    const capitalize = (string) => {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    };
-    const pathPrefix = config.pathPrefix ? config.pathPrefix : "/";
-    const currentPath = this.props.location.pathname
-      .replace(pathPrefix, "")
-      .replace("/", "");
-    let title = "";
-    if (currentPath === "") {
-      title = "Home";
-    } else if (currentPath.includes("docs/")) {
-      const doc = currentPath
-        .replace("docs/", "")
-        .replace("/", "")
-        .replace("-", " ");
-      title = `${capitalize(doc)}`;
-    }
-    return title;
-  }
   render() {
     const { children } = this.props;
 
     return (
       <div>
         <Helmet>
-          <title>{`${config.siteTitle} |  ${this.getLocalTitle()}`}</title>
+          <title>{config.siteTitle}</title>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
         {children()}
