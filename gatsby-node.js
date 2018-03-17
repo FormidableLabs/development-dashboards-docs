a9e9e06ee3dbf302c41054ec4f426d65cc9de868
Paula Lavalle <lavalle.paula@gmail.com>
2018-03-16 17:41:12 -0700

    Remove sourcemaps in production

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,27 @@
-exports.modifyWebpackConfig = ({ config }) => {
+exports.modifyWebpackConfig = ({ config, stage }) => {
   // Do not transform SVG into data-uris
-  config.loader('url-loader', {
+  config.loader("url-loader", {
     test: /\.(jpg|jpeg|png|gif|mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
-    loader: 'url',
+    loader: "url",
     query: {
       limit: 10000,
-      name: 'static/[name].[hash:8].[ext]'
+      name: "static/[name].[hash:8].[ext]"
     }
   });
 
   // Instead load <svg> elements directly into the DOM
-  config.loader('raw-loader', {
+  config.loader("raw-loader", {
     test: /\.(svg)(\?.*)?$/,
-    loader: 'raw'
+    loader: "raw"
   });
+
+  // Remove sourcemaps for production
+  if (stage !== "develop") {
+    if (config.devtool) {
+      config.devtool = false;
+    }
+    if (config._config.devtool) {
+      config._config.devtool = false;
+    }
+  }
 };
