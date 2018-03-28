6530cc2db4e01ceec1ea60eea77367021f9c4808
Paula Lavalle <lavalle.paula@gmail.com>
2018-03-28 11:19:10 -0700

    Fix font loading by moving it to gatsby-browser

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-import FontFaceObserver from "fontfaceobserver";
 import { Footer } from "formidable-landers";
 
 import config from "../../data/site-config";
@@ -16,45 +15,6 @@ if (
 }
 
 class Layout extends React.Component {
-  componentDidMount() {
-    // "Flash of Faux Text": Add class to body once font is loaded
-    const lazer84Observer = new FontFaceObserver("lazer84", {
-      style: "italic"
-    });
-    const montserratObserver = new FontFaceObserver("Montserrat");
-    const exoObserver = new FontFaceObserver("exo");
-
-    lazer84Observer
-      .load()
-      .then(() => {
-        document.documentElement.classList.add("lazer84-active");
-      })
-      .catch((err) => {
-        /* eslint no-console: "off"*/
-        console.warn("lazer84 failed to load.", err);
-      });
-
-    montserratObserver
-      .load()
-      .then(() => {
-        document.documentElement.classList.add("montserrat-active");
-      })
-      .catch((err) => {
-        /* eslint no-console: "off"*/
-        console.warn("montserrat failed to load.", err);
-      });
-
-    exoObserver
-      .load()
-      .then(() => {
-        document.documentElement.classList.add("exo-active");
-      })
-      .catch((err) => {
-        /* eslint no-console: "off"*/
-        console.warn("exo failed to load.", err);
-      });
-  }
-
   render() {
     const { children } = this.props;
 
