f01ca31a97647ade62603bf67fedb29cb72592ca
Paula Lavalle <paulathevalley@users.noreply.github.com>
2018-03-29 09:14:12 -0700

    Bug: Fix font loading (#28)
    * Fix font loading by moving it to gatsby-browser
    
    * Use require
    
    * Upgrade gatsby to a newer version
    
    * Add gatsby-plugin-postcss-sass for resolving url() in css
    
    * Fix asset url() paths

---
+++ gatsby-browser.js
@@ -0,0 +1,42 @@
+const FontFaceObserver = require("fontfaceobserver");
+
+exports.onClientEntry = () => {
+  // Called when the Gatsby browser runtime first starts.
+
+  // "Flash of Faux Text": Add class to body once font is loaded
+  const lazer84Observer = new FontFaceObserver("lazer84", {
+    style: "italic"
+  });
+  const montserratObserver = new FontFaceObserver("Montserrat");
+  const exoObserver = new FontFaceObserver("exo");
+
+  lazer84Observer
+    .load()
+    .then(() => {
+      document.documentElement.classList.add("lazer84-active");
+    })
+    .catch(err => {
+      /* eslint no-console: "off"*/
+      console.warn("lazer84 failed to load.", err);
+    });
+
+  montserratObserver
+    .load()
+    .then(() => {
+      document.documentElement.classList.add("montserrat-active");
+    })
+    .catch(err => {
+      /* eslint no-console: "off"*/
+      console.warn("montserrat failed to load.", err);
+    });
+
+  exoObserver
+    .load()
+    .then(() => {
+      document.documentElement.classList.add("exo-active");
+    })
+    .catch(err => {
+      /* eslint no-console: "off"*/
+      console.warn("exo failed to load.", err);
+    });
+};
